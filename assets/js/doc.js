4cffeedd002d4667846536015c85497ea4d0610e
Shubham-391 <subhamy1234@gmail.com>
2023-11-15 12:45:04 +0530

    hove

--- assets/js/doc.js
+++ assets/js/doc.js
@@ -2,22 +2,23 @@ const add = document.querySelector(".add"), save = document.querySelector(".save
 let value = document.querySelector(".value"), show = document.querySelector(".print");;
 add.addEventListener("click", function () {
     value.innerHTML = +value.innerHTML + 1;
+    save.classList.remove("not-allowed");
 });
 
 reset.addEventListener("click", function () {
     value.innerHTML = 0;
     show.innerHTML = "";
-
+    save.classList.add("not-allowed");
 });
 save.addEventListener("click", function () {
     if (value.innerHTML == 0) {
         save.classList.add("not-allowed");
     } else if (show.innerHTML == "") {
         save.classList.remove("not-allowed");
-        show.innerHTML = value.innerHTML; 
+        show.innerHTML = value.innerHTML;
     } else {
         save.classList.remove("not-allowed");
         show.innerHTML = show.innerHTML + "," + value.innerHTML;
     }
     value.innerHTML = 0;
-})
+});
